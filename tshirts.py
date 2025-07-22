6152e3453872da7dc04c02e60ce42057e14f9823
Sudeep Prasad <sudeep.rp@gmail.com>
2025-07-22 16:05:19 +0530

    fix tshirts

--- tshirts.py
+++ tshirts.py
@@ -2,13 +2,12 @@
 def size(cms):
     if cms < 38:
         return 'S'
-    elif cms > 38 and cms < 42:
+    elif cms < 42:
         return 'M'
     else:
         return 'L'
 
 
-assert(size(5) == 'error')
 assert(size(37) == 'S')
 assert(size(38) == 'M')  # we are asserting that our customer prefers a relaxed fit
 assert(size(40) == 'M')
